3dbfabe500a477960a3bccdff022e9f573d66374
Jean-Christian Denis <contact@jcdenis.fr>
2025-08-24 18:50:59 +0200

    release 0.2.1

--- _define.php
+++ _define.php
@@ -18,7 +18,7 @@ $this->registerModule(
     'Frontend webauthn',
     'Use passkey on frontend.',
     'Jean-Christian Paul Denis and Contributors',
-    '0.2',
+    '0.2.1',
     [
         'requires'    => [
             ['core', '2.36'],
